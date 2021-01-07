c2e33324b9a54f70f8a0e030aeb4775fb8bac0d9
Vladimir <vladimir.khorikov@gmail.com>
2021-01-07 08:45:48 -0500

    Validating the manual (naive) way

--- src/Api/StudentController.cs
+++ src/Api/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using DomainModel;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,7 +12,8 @@ namespace Api
         private readonly StudentRepository _studentRepository;
         private readonly CourseRepository _courseRepository;
 
-        public StudentController(StudentRepository studentRepository, CourseRepository courseRepository)
+        public StudentController(
+            StudentRepository studentRepository, CourseRepository courseRepository)
         {
             _studentRepository = studentRepository;
             _courseRepository = courseRepository;
@@ -20,6 +22,30 @@ namespace Api
         [HttpPost]
         public IActionResult Register([FromBody] RegisterRequest request)
         {
+            if (request == null)
+                return BadRequest("Request cannot be empty");
+            
+            if (string.IsNullOrWhiteSpace(request.Name))
+                return BadRequest("Name cannot be empty");
+            if (request.Name.Length > 200)
+                return BadRequest("Name is too long");
+
+            if (string.IsNullOrWhiteSpace(request.Email))
+                return BadRequest("Email cannot be empty");
+            if (request.Email.Length > 150)
+                return BadRequest("Email is too long");
+            if (Regex.IsMatch(request.Email, @"^(.+)@(.+)$") == false)
+                return BadRequest("Email is invalid");
+            // Email should be unique
+
+            if (string.IsNullOrWhiteSpace(request.Address))
+                return BadRequest("Address cannot be empty");
+            if (request.Address.Length > 150)
+                return BadRequest("Address is too long");
+            
+            // Error codes
+            // Return a list of errors, not just the first one
+
             var student = new Student(request.Email, request.Name, request.Address);
             _studentRepository.Save(student);
 
@@ -33,6 +59,8 @@ namespace Api
         [HttpPut("{id}")]
         public IActionResult EditPersonalInfo(long id, [FromBody] EditPersonalInfoRequest request)
         {
+            // Same validations here
+            // Check that the student exists
             Student student = _studentRepository.GetById(id);
 
             student.EditPersonalInfo(request.Name, request.Address);
@@ -44,6 +72,10 @@ namespace Api
         [HttpPost("{id}/enrollments")]
         public IActionResult Enroll(long id, [FromBody] EnrollRequest request)
         {
+            // Check that the student exists
+            // Check that the courses exist
+            // Grade is correctly parsed
+            // Business rules in the Enroll method
             Student student = _studentRepository.GetById(id);
 
             foreach (CourseEnrollmentDto enrollmentDto in request.Enrollments)
--- src/Api/StudentRepository.cs
+++ src/Api/StudentRepository.cs
@@ -41,7 +41,10 @@ namespace Api
 
         private static Student Alice()
         {
-            var alice = new Student("alice@gmail.com", "Alice Alison", "1234 Main St, Arlington, VA, 22201");
+            var alice = new Student(
+                "alice@gmail.com",
+                "Alice Alison",
+                "1234 Main St, Arlington, VA, 22201");
             SetId(alice, 1);
             alice.Enroll(new Course(1, "Calculus", 5), Grade.A);
 
@@ -50,7 +53,10 @@ namespace Api
 
         private static Student Bob()
         {
-            var bob = new Student("bob@gmail.com", "Bob Bobson", "2345 Second St, Barlington, VA, 22202");
+            var bob = new Student(
+                "bob@gmail.com",
+                "Bob Bobson",
+                "2345 Second St, Barlington, VA, 22202");
             SetId(bob, 2);
             bob.Enroll(new Course(2, "History", 4), Grade.B);
             
